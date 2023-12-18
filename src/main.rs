3e9c4c9c77a5042f3d3e0b0b87e267f423f3a41a
Adi-df <adidf-web@laposte.net>
2023-12-18 20:53:55 +0100

    Use better logging

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::{self, ReadDir};
 use std::io;
 use std::path::PathBuf;
 
+use log::{error, trace};
+
 use dirs;
 
 use clap::{Parser, Subcommand};
@@ -28,27 +30,34 @@ fn main() {
         if let Some(data_dir) = dirs::data_dir() {
             data_dir.join("foucault")
         } else {
+            error!("User data directory is unavailable");
             unimplemented!();
         }
     };
 
-    let app_dir: ReadDir = match fs::read_dir(&app_dir_path) {
-        Ok(dir) => dir,
-        Err(err) if matches!(err.kind(), io::ErrorKind::NotFound) => {
-            fs::create_dir(&app_dir_path).unwrap();
-            fs::read_dir(&app_dir_path).unwrap()
+    let app_dir: ReadDir = fs::read_dir(&app_dir_path).unwrap_or_else(|err| {
+        if matches!(err.kind(), io::ErrorKind::NotFound) {
+            if fs::create_dir(&app_dir_path).is_err() {
+                error!("Unable to create the app directory");
+            }
+            fs::read_dir(&app_dir_path).unwrap_or_else(|err| {
+                error!("Couldn't read the just created app directory. Should be unreachable.");
+                panic!("{}", err.to_string());
+            })
+        } else {
+            error!("Unknown error occured while opening the app directory");
+            panic!("{}", err.to_string());
         }
-        _ => unimplemented!(),
-    };
+    });
 
     let cli = Cli::parse();
 
     if let Some(command) = &cli.command {
         match command {
-            Commands::Create { name } => println!("Create notebook {name}"),
-            Commands::Open { name } => println!("Open notebook {name}"),
+            Commands::Create { name } => trace!("Create notebook {name}"),
+            Commands::Open { name } => trace!("Open notebook {name}"),
         }
     } else {
-        println!("Open default notebook manager");
+        trace!("Open default notebook manager");
     }
 }
