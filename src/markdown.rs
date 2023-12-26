ef0af07dd4211cd78d2bdc3304391c0e4b2da719
Adi-df <adidf-web@laposte.net>
2023-12-26 13:18:54 +0100

    Rework rendering system to be more adaptable

--- src/markdown.rs
+++ src/markdown.rs
@@ -1,13 +1,15 @@
-use markdown::mdast::Node;
-use markdown::{to_mdast, ParseOptions};
+mod elements;
 
+use markdown::{to_mdast, ParseOptions};
 use ratatui::{
     prelude::Alignment,
-    style::{Color, Modifier, Style, Stylize},
-    text::{Line, Span},
+    style::{Color, Modifier},
+    text::Line,
 };
 
-static HEADER_COLOR: [Color; 6] = [
+use self::elements::BlockElements;
+
+const HEADER_COLOR: [Color; 6] = [
     Color::Red,
     Color::Green,
     Color::Blue,
@@ -15,7 +17,7 @@ static HEADER_COLOR: [Color; 6] = [
     Color::Magenta,
     Color::Cyan,
 ];
-static HEADER_MODIFIER: [Modifier; 6] = [
+const HEADER_MODIFIER: [Modifier; 6] = [
     Modifier::BOLD,
     Modifier::empty(),
     Modifier::ITALIC,
@@ -23,7 +25,7 @@ static HEADER_MODIFIER: [Modifier; 6] = [
     Modifier::DIM,
     Modifier::DIM,
 ];
-static HEADER_ALIGNEMENT: [Alignment; 6] = [
+const HEADER_ALIGNEMENT: [Alignment; 6] = [
     Alignment::Center,
     Alignment::Center,
     Alignment::Left,
@@ -32,14 +34,16 @@ static HEADER_ALIGNEMENT: [Alignment; 6] = [
     Alignment::Left,
 ];
 
-static TEXT: usize = 0;
-static ITALIC: usize = 1;
-static STRONG: usize = 2;
-static LINK: usize = 3;
-static CROSS_REF: usize = 4;
-static BLOCKQUOTE: usize = 5;
+const BLOCKQUOTE_ALIGNEMENT: Alignment = Alignment::Center;
 
-static RICH_TEXT_COLOR: [Color; 6] = [
+const TEXT: usize = 0;
+const ITALIC: usize = 1;
+const STRONG: usize = 2;
+const HYPERLINK: usize = 3;
+const CROSS_REF: usize = 4;
+const BLOCKQUOTE: usize = 5;
+
+const RICH_TEXT_COLOR: [Color; 6] = [
     Color::Reset,     // Text
     Color::Green,     // Italic
     Color::Yellow,    // Strong
@@ -52,123 +56,9 @@ pub fn links(content: &str) -> Vec<String> {
     todo!();
 }
 
-pub fn render<'a>(content: &str) -> Vec<Line<'a>> {
-    parse(&to_mdast(content, &ParseOptions::default()).unwrap())
-}
-
-fn parse<'a>(ast: &Node) -> Vec<Line<'a>> {
-    match ast {
-        Node::Root(root) => root.children.iter().flat_map(parse).collect(),
-        Node::Paragraph(paragraph) => vec![Line::from(
-            paragraph
-                .children
-                .iter()
-                .flat_map(parse_text)
-                .collect::<Vec<Span>>(),
-        )],
-        Node::Heading(header) => {
-            let depth = header.depth as usize - 1;
-            vec![Line::from(
-                header
-                    .children
-                    .iter()
-                    .flat_map(parse_text)
-                    .map(|text| {
-                        text.style(
-                            Style::default()
-                                .fg(HEADER_COLOR[depth])
-                                .add_modifier(HEADER_MODIFIER[depth] | Modifier::UNDERLINED),
-                        )
-                    })
-                    .collect::<Vec<Span>>(),
-            )
-            .alignment(HEADER_ALIGNEMENT[depth])]
-        }
-        Node::BlockQuote(block) => block
-            .children
-            .iter()
-            .flat_map(parse)
-            .map(|line| {
-                let mut line = line.alignment(Alignment::Center);
-                line.patch_style(
-                    Style::default()
-                        .fg(RICH_TEXT_COLOR[BLOCKQUOTE])
-                        .add_modifier(Modifier::ITALIC),
-                );
-                line
-            })
-            .collect(),
-        _ => Vec::new(),
-    }
-}
-
-fn parse_text<'a>(child: &Node) -> Vec<Span<'a>> {
-    match child {
-        Node::Text(text) => parse_cross_links(text.value.as_str()),
-        Node::Emphasis(empathis) => empathis
-            .children
-            .iter()
-            .flat_map(parse_text)
-            .map(|span| {
-                span.add_modifier(Modifier::ITALIC)
-                    .fg(RICH_TEXT_COLOR[ITALIC])
-            })
-            .collect(),
-        Node::Strong(strong) => strong
-            .children
-            .iter()
-            .flat_map(parse_text)
-            .map(|span| {
-                span.add_modifier(Modifier::BOLD)
-                    .fg(RICH_TEXT_COLOR[STRONG])
-            })
-            .collect(),
-        Node::Link(link) => link
-            .children
-            .iter()
-            .flat_map(parse_text)
-            .map(|span| {
-                span.style(
-                    Style::default()
-                        .fg(RICH_TEXT_COLOR[LINK])
-                        .add_modifier(Modifier::UNDERLINED),
-                )
-            })
-            .collect(),
-        _ => Vec::new(),
-    }
-}
-
-fn parse_cross_links<'a>(content: &str) -> Vec<Span<'a>> {
-    let mut content_iter = content.chars().peekable();
-    let mut escape = false;
-    let mut spans = Vec::new();
-    let mut current_span = String::new();
-
-    while let Some(c) = content_iter.next() {
-        if escape {
-            current_span.push(c);
-            escape = false;
-            continue;
-        }
-
-        if c == '[' && matches!(content_iter.peek(), Some('[')) {
-            spans.push(Span::raw(current_span).style(Style::default().fg(RICH_TEXT_COLOR[TEXT])));
-            current_span = String::from("[");
-            content_iter.next();
-        } else if c == ']' && matches!(content_iter.peek(), Some(']')) {
-            current_span.push(']');
-            spans.push(Span::raw(current_span).style(Style::new().fg(RICH_TEXT_COLOR[CROSS_REF])));
-            current_span = String::new();
-            content_iter.next();
-        } else {
-            current_span.push(c);
-        }
-    }
-
-    if !current_span.is_empty() {
-        spans.push(Span::raw(current_span).style(Style::default().fg(RICH_TEXT_COLOR[TEXT])));
-    }
-
-    spans
+pub fn render(content: &str) -> Vec<Line<'static>> {
+    BlockElements::parse_node(&to_mdast(content, &ParseOptions::default()).unwrap())
+        .into_iter()
+        .flat_map(BlockElements::into_line)
+        .collect()
 }
---
+++ src/markdown/elements.rs
@@ -0,0 +1,251 @@
+use markdown::mdast;
+use ratatui::{
+    style::{Modifier, Style},
+    text::{Line, Span},
+};
+
+use crate::markdown::{
+    BLOCKQUOTE, BLOCKQUOTE_ALIGNEMENT, CROSS_REF, HEADER_ALIGNEMENT, HEADER_COLOR, HEADER_MODIFIER,
+    HYPERLINK, ITALIC, RICH_TEXT_COLOR, STRONG, TEXT,
+};
+
+const TEXT_STYLE: Style = Style::new().fg(RICH_TEXT_COLOR[TEXT]);
+
+const ITALIC_STYLE: Style = Style::new()
+    .add_modifier(Modifier::UNDERLINED)
+    .fg(RICH_TEXT_COLOR[ITALIC]);
+
+const STRONG_STYLE: Style = Style::new()
+    .add_modifier(Modifier::BOLD)
+    .fg(RICH_TEXT_COLOR[STRONG]);
+
+const HYPER_LINK_STYLE: Style = Style::new()
+    .add_modifier(Modifier::UNDERLINED)
+    .fg(RICH_TEXT_COLOR[HYPERLINK]);
+
+const CROSS_REF_STYLE: Style = Style::new().fg(RICH_TEXT_COLOR[CROSS_REF]);
+
+const BLOCKQUOTE_STYLE: Style = Style::new()
+    .fg(RICH_TEXT_COLOR[BLOCKQUOTE])
+    .add_modifier(Modifier::ITALIC);
+
+const HEADING_STYLE: [Style; 6] = [
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[0], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[0]),
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[1], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[1]),
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[2], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[2]),
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[3], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[3]),
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[4], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[4]),
+    Style::new()
+        .add_modifier(Modifier::union(HEADER_MODIFIER[5], Modifier::UNDERLINED))
+        .fg(HEADER_COLOR[5]),
+];
+
+pub enum InlineElements {
+    RichText { span: Span<'static> },
+    HyperLink { span: Span<'static>, dest: String },
+    CrossRef { span: Span<'static>, dest: String },
+}
+
+impl InlineElements {
+    pub fn parse_node(node: &mdast::Node) -> Vec<InlineElements> {
+        match node {
+            mdast::Node::Emphasis(italic) => italic
+                .children
+                .iter()
+                .flat_map(InlineElements::parse_node)
+                .map(|el| el.richify(ITALIC_STYLE))
+                .collect(),
+            mdast::Node::Strong(strong) => strong
+                .children
+                .iter()
+                .flat_map(InlineElements::parse_node)
+                .map(|el| el.richify(STRONG_STYLE))
+                .collect(),
+            mdast::Node::Link(link) => vec![InlineElements::HyperLink {
+                span: Span::raw(
+                    link.children
+                        .iter()
+                        .flat_map(InlineElements::parse_node)
+                        .map(InlineElements::discard_style)
+                        .collect::<String>(),
+                )
+                .style(HYPER_LINK_STYLE),
+                dest: link.url.to_string(),
+            }],
+            mdast::Node::Text(text) => parse_cross_links(text.value.as_str()),
+            _ => Vec::new(),
+        }
+    }
+
+    fn richify(self, style: Style) -> Self {
+        match self {
+            Self::RichText { mut span } => {
+                span.patch_style(style);
+                Self::RichText { span }
+            }
+            s @ _ => s,
+        }
+    }
+
+    fn discard_style(self) -> String {
+        match self {
+            Self::RichText { span } => span.content.to_string(),
+            Self::CrossRef { span, .. } => span.content.to_string(),
+            Self::HyperLink { span, .. } => span.content.to_string(),
+        }
+    }
+
+    fn into_span(self) -> Span<'static> {
+        match self {
+            InlineElements::RichText { span } => span,
+            InlineElements::HyperLink { span, .. } => span,
+            InlineElements::CrossRef { span, .. } => span,
+        }
+    }
+}
+
+pub enum BlockElements {
+    Paragraph {
+        content: Vec<InlineElements>,
+    },
+    Heading {
+        content: Vec<InlineElements>,
+        level: u8,
+    },
+    BlockQuote {
+        content: Vec<InlineElements>,
+    },
+}
+
+impl BlockElements {
+    pub fn parse_node(node: &mdast::Node) -> Vec<BlockElements> {
+        match node {
+            mdast::Node::Root(root) => root
+                .children
+                .iter()
+                .flat_map(BlockElements::parse_node)
+                .collect(),
+            mdast::Node::BlockQuote(blockquote) => vec![Self::BlockQuote {
+                content: blockquote
+                    .children
+                    .iter()
+                    .flat_map(BlockElements::parse_node)
+                    .flat_map(BlockElements::content)
+                    .collect(),
+            }],
+            mdast::Node::Heading(heading) => vec![Self::Heading {
+                level: heading.depth - 1,
+                content: heading
+                    .children
+                    .iter()
+                    .flat_map(InlineElements::parse_node)
+                    .collect(),
+            }],
+            mdast::Node::Paragraph(paragraph) => vec![Self::Paragraph {
+                content: paragraph
+                    .children
+                    .iter()
+                    .flat_map(InlineElements::parse_node)
+                    .collect(),
+            }],
+            _ => Vec::new(),
+        }
+    }
+
+    fn content(self) -> Vec<InlineElements> {
+        match self {
+            BlockElements::Paragraph { content } => content,
+            BlockElements::Heading { content, .. } => content,
+            BlockElements::BlockQuote { content } => content,
+        }
+    }
+
+    pub fn into_line(self) -> Vec<Line<'static>> {
+        match self {
+            Self::Paragraph { content } => {
+                vec![Line::from(
+                    content
+                        .into_iter()
+                        .map(InlineElements::into_span)
+                        .collect::<Vec<Span<'static>>>(),
+                )]
+            }
+            BlockElements::Heading { content, level } => vec![Line::from(
+                content
+                    .into_iter()
+                    .map(|el| el.richify(HEADING_STYLE[level as usize]))
+                    .map(InlineElements::into_span)
+                    .collect::<Vec<Span<'static>>>(),
+            )
+            .alignment(HEADER_ALIGNEMENT[level as usize])],
+            BlockElements::BlockQuote { content } => vec![Line::from(
+                content
+                    .into_iter()
+                    .map(|el| el.richify(BLOCKQUOTE_STYLE))
+                    .map(InlineElements::into_span)
+                    .collect::<Vec<Span<'static>>>(),
+            )
+            .alignment(BLOCKQUOTE_ALIGNEMENT)],
+        }
+    }
+}
+
+fn parse_cross_links(text: &str) -> Vec<InlineElements> {
+    let mut content_iter = text.chars().peekable();
+    let mut escape = false;
+    let mut cross_ref = false;
+    let mut current_span = String::new();
+    let mut spans = Vec::new();
+
+    while let Some(c) = content_iter.next() {
+        if cross_ref {
+            if c == ']' && matches!(content_iter.peek(), Some(']')) {
+                spans.push(InlineElements::CrossRef {
+                    span: Span::raw(format!("[{}]", current_span)).style(CROSS_REF_STYLE),
+                    dest: current_span,
+                });
+                current_span = String::new();
+                cross_ref = false;
+                content_iter.next();
+            } else {
+                current_span.push(c)
+            }
+        } else {
+            if escape {
+                current_span.push(c);
+                escape = false;
+                continue;
+            }
+
+            if c == '[' && matches!(content_iter.peek(), Some('[')) {
+                spans.push(InlineElements::RichText {
+                    span: Span::raw(current_span).style(TEXT_STYLE),
+                });
+                current_span = String::new();
+                cross_ref = true;
+
+                content_iter.next();
+            } else {
+                current_span.push(c);
+            }
+        }
+    }
+
+    if !current_span.is_empty() {
+        spans.push(InlineElements::RichText {
+            span: Span::raw(current_span),
+        })
+    }
+
+    spans
+}
