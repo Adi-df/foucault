5fb591b0b95c5847807d3243632fa17a679872f4
Adrien Degliame <adidf-web@laposte.net>
2024-08-21 11:03:38 +0200

    Use a similar result system for note_api

--- foucault-server/src/note_api.rs
+++ foucault-server/src/note_api.rs
@@ -1,3 +1,5 @@
+use log::error;
+
 use axum::{
     extract::{Json, State},
     http::StatusCode,
@@ -9,6 +11,7 @@ use serde_error::Error;
 use crate::{
     link_repr::Link,
     note_repr::{self, Note, NoteError, NoteSummary},
+    tag_api::FailibleJsonResult,
     tag_repr::{Tag, TagError},
     AppState,
 };
@@ -58,16 +61,17 @@ pub struct RemoveTagParam {
 pub(crate) async fn create(
     State(state): State<AppState>,
     Json(CreateParam { name, content }): Json<CreateParam>,
-) -> (StatusCode, Json<Result<i64, NoteError>>) {
+) -> FailibleJsonResult<Result<i64, NoteError>> {
     let res = note_repr::create(&name, &content, state.notebook.db()).await;
 
     match res {
-        Ok(id) => (StatusCode::OK, Json::from(Ok(id))),
+        Ok(id) => Ok((StatusCode::OK, Json::from(Ok(id)))),
         Err(err) => {
             if let Some(note_err) = err.downcast_ref::<NoteError>() {
-                (StatusCode::NOT_ACCEPTABLE, Json::from(Err(*note_err)))
+                Ok((StatusCode::NOT_ACCEPTABLE, Json::from(Err(*note_err))))
             } else {
-                panic!("Error encountered during note creation : {err}");
+                error!("Error encountered during note creation : {err}");
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
             }
         }
     }
@@ -76,122 +80,156 @@ pub(crate) async fn create(
 pub(crate) async fn validate_name(
     State(state): State<AppState>,
     Json(name): Json<String>,
-) -> (StatusCode, Json<Option<NoteError>>) {
-    let res = note_repr::validate_name(&name, state.notebook.db())
-        .await
-        .expect("Error encountered during name validation");
+) -> FailibleJsonResult<Option<NoteError>> {
+    let res = note_repr::validate_name(&name, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered during name validation : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn load_by_id(
     State(state): State<AppState>,
     Json(id): Json<i64>,
-) -> (StatusCode, Json<Option<Note>>) {
-    let res = note_repr::load_by_id(id, state.notebook.db())
-        .await
-        .expect("Error encountered during note loading");
+) -> FailibleJsonResult<Option<Note>> {
+    let res = note_repr::load_by_id(id, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered during note loading : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn load_by_name(
     State(state): State<AppState>,
     Json(name): Json<String>,
-) -> (StatusCode, Json<Option<Note>>) {
-    let res = note_repr::load_by_name(&name, state.notebook.db())
-        .await
-        .expect("Error encountered during note loading");
+) -> FailibleJsonResult<Option<Note>> {
+    let res = note_repr::load_by_name(&name, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered during note loading : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn rename(
     State(state): State<AppState>,
     Json(RenameParam { id, name }): Json<RenameParam>,
-) -> (StatusCode, Json<Option<NoteError>>) {
+) -> FailibleJsonResult<Option<NoteError>> {
     let res = note_repr::rename(id, &name, state.notebook.db()).await;
 
     match res {
-        Ok(()) => (StatusCode::OK, Json::from(None)),
+        Ok(()) => Ok((StatusCode::OK, Json::from(None))),
         Err(err) => {
             if let Some(note_err) = err.downcast_ref::<NoteError>() {
-                (StatusCode::NOT_ACCEPTABLE, Json::from(Some(*note_err)))
+                Ok((StatusCode::NOT_ACCEPTABLE, Json::from(Some(*note_err))))
             } else {
-                panic!("Error encountered during note renaming : {err}");
+                error!("Error encountered during note renaming : {err}");
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
             }
         }
     }
 }
 
 pub(crate) async fn delete(State(state): State<AppState>, Json(id): Json<i64>) -> StatusCode {
-    note_repr::delete(id, state.notebook.db())
-        .await
-        .expect("Error encountered when deleting note");
+    let res = note_repr::delete(id, state.notebook.db()).await;
 
-    StatusCode::OK
+    match res {
+        Ok(()) => StatusCode::OK,
+        Err(err) => {
+            error!("Error encountered when deleting note : {err}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 pub(crate) async fn update_content(
     State(state): State<AppState>,
     Json(UpdateContentParam { id, content }): Json<UpdateContentParam>,
 ) -> StatusCode {
-    note_repr::update_content(id, &content, state.notebook.db())
-        .await
-        .expect("Error encountered when updating note content");
+    let res = note_repr::update_content(id, &content, state.notebook.db()).await;
 
-    StatusCode::OK
+    match res {
+        Ok(()) => StatusCode::OK,
+        Err(err) => {
+            error!("Error encountered when updating note content : {err}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 pub(crate) async fn update_links(
     State(state): State<AppState>,
     Json(UpdateLinksParam { id, links }): Json<UpdateLinksParam>,
 ) -> StatusCode {
-    note_repr::update_links(id, &links, state.notebook.db())
-        .await
-        .expect("Error encountered when updating note links");
+    let res = note_repr::update_links(id, &links, state.notebook.db()).await;
 
-    StatusCode::OK
+    match res {
+        Ok(()) => StatusCode::OK,
+        Err(err) => {
+            error!("Error encountered when updating note links : {err}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 pub(crate) async fn list_tags(
     State(state): State<AppState>,
     Json(id): Json<i64>,
-) -> (StatusCode, Json<Vec<Tag>>) {
-    let res = note_repr::list_tags(id, state.notebook.db())
-        .await
-        .expect("Error encountered while listing note's tags");
+) -> FailibleJsonResult<Vec<Tag>> {
+    let res = note_repr::list_tags(id, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered while listing note's tags : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn validate_new_tag(
     State(state): State<AppState>,
     Json(ValidateNewTagParam { id, tag_id }): Json<ValidateNewTagParam>,
-) -> (StatusCode, Json<Option<Error>>) {
-    let res = note_repr::validate_new_tag(id, tag_id, state.notebook.db())
-        .await
-        .expect("Error encountered during tag validation");
+) -> FailibleJsonResult<Option<Error>> {
+    let res = note_repr::validate_new_tag(id, tag_id, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res.map(|err| Error::new(&*err))))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res.map(|err| Error::new(&*err))))),
+        Err(err) => {
+            error!("Error encountered during tag validation : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn add_tag(
     State(state): State<AppState>,
     Json(AddTagParam { id, tag_id }): Json<AddTagParam>,
-) -> (StatusCode, Json<Option<Error>>) {
+) -> FailibleJsonResult<Option<Error>> {
     let res = note_repr::add_tag(id, tag_id, state.notebook.db()).await;
 
     match res {
-        Ok(()) => (StatusCode::OK, Json::from(None)),
+        Ok(()) => Ok((StatusCode::OK, Json::from(None))),
         Err(err) => {
             if err.is::<NoteError>() || err.is::<TagError>() {
-                (
+                Ok((
                     StatusCode::NOT_ACCEPTABLE,
                     Json::from(Some(Error::new(&*err))),
-                )
+                ))
             } else {
-                panic!("Error encountered while adding tag : {err}");
+                error!("Error encountered while adding tag : {err}");
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
             }
         }
     }
@@ -201,31 +239,43 @@ pub(crate) async fn remove_tag(
     State(state): State<AppState>,
     Json(RemoveTagParam { id, tag_id }): Json<RemoveTagParam>,
 ) -> StatusCode {
-    note_repr::remove_tag(id, tag_id, state.notebook.db())
-        .await
-        .expect("Error encountered while removing tag");
+    let res = note_repr::remove_tag(id, tag_id, state.notebook.db()).await;
 
-    StatusCode::OK
+    match res {
+        Ok(()) => StatusCode::OK,
+        Err(err) => {
+            error!("Error encountered while removing tag : {err}");
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
 
 pub(crate) async fn search_by_name(
     State(state): State<AppState>,
     Json(pattern): Json<String>,
-) -> (StatusCode, Json<Vec<NoteSummary>>) {
-    let res = note_repr::search_by_name(&pattern, state.notebook.db())
-        .await
-        .expect("Error encountered when searching notes");
+) -> FailibleJsonResult<Vec<NoteSummary>> {
+    let res = note_repr::search_by_name(&pattern, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered when searching notes : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 pub(crate) async fn search_by_tag(
     State(state): State<AppState>,
     Json(tag_id): Json<i64>,
-) -> (StatusCode, Json<Vec<NoteSummary>>) {
-    let res = note_repr::search_by_tag(tag_id, state.notebook.db())
-        .await
-        .expect("Error encountered while fetching notes summaries");
+) -> FailibleJsonResult<Vec<NoteSummary>> {
+    let res = note_repr::search_by_tag(tag_id, state.notebook.db()).await;
 
-    (StatusCode::OK, Json::from(res))
+    match res {
+        Ok(res) => Ok((StatusCode::OK, Json::from(res))),
+        Err(err) => {
+            error!("Error encountered while fetching notes summaries : {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
