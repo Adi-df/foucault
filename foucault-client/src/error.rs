4884d28fae567e81c729782ea20e51c108ae3300
Adrien Degliame <adidf-web@laposte.net>
2024-08-22 11:30:16 +0200

    Add more diverse error codes and messages

--- foucault-client/src/error.rs
+++ foucault-client/src/error.rs
@@ -3,9 +3,13 @@ use thiserror::Error;
 use reqwest::{Response, StatusCode};
 
 #[derive(Debug, Error)]
-#[error("The request returned unexpected status code : {response_code}")]
-pub struct ResponseCodeError {
-    response_code: StatusCode,
+pub enum ResponseCodeError {
+    #[error("Such operation isn't allowed")]
+    Unauthorized,
+    #[error("The server encountered an internal error")]
+    InternalServerError,
+    #[error("Unexpected response code : {0}")]
+    UnexpectedCode(StatusCode),
 }
 
 pub trait TryResponseCode {
@@ -20,8 +24,10 @@ impl TryResponseCode for Response {
         Self: Sized,
     {
         match self.status() {
-            StatusCode::OK => Ok(self),
-            response_code @ _ => Err(ResponseCodeError { response_code }),
+            StatusCode::OK | StatusCode::NOT_ACCEPTABLE => Ok(self),
+            StatusCode::UNAUTHORIZED => Err(ResponseCodeError::Unauthorized),
+            StatusCode::INTERNAL_SERVER_ERROR => Err(ResponseCodeError::InternalServerError),
+            status @ _ => Err(ResponseCodeError::UnexpectedCode(status)),
         }
     }
 }
